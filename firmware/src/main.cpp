268e8ea497a0e42ec97f2afaa091e19c23308b48
Raul Aquino <aquinoraul207@gmail.com>
2021-07-22 11:55:16 -0300

    anti zika hello world implemented

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,8 +2,17 @@
 
 void setup() {
   // put your setup code here, to run once:
+  while (!Serial)
+  {
+    /* code */
+  }
+
+  Serial.begin(9600);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Hello World");
+  delay(1000);
 }
