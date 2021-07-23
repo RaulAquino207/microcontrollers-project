f52a39b806d6c12e6008015ff3be5c93a9278d1f
Raul Aquino <aquinoraul207@gmail.com>
2021-07-23 00:47:35 -0300

    Done some tests using common code for arduino

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include "OneButton.h"
+
+#define pinBotao 2
+#define pinLED 1
 
 void setup() {
-  // put your setup code here, to run once:
   while (!Serial)
   {
     /* code */
   }
 
   Serial.begin(9600);
+  pinMode(pinLED, OUTPUT);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Hello World");
-  delay(1000);
+
+  digitalWrite(pinLED, HIGH);
+  delay(3000);
+
+  digitalWrite(pinLED, LOW);
+  delay(3000);
 }
