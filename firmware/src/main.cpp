63771f3234d1ebd150b8e5857b8cd7e4ab59310b
Raul Aquino <aquinoraul207@gmail.com>
2021-07-25 14:07:49 -0300

    Two buttons working using the platform IO Onebutton

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,25 +1,43 @@
 #include <Arduino.h>
+#include <OneButton.h>
 
-#define LED 1
-#define Button1 23
+OneButton button1(17, true);
+OneButton button2(23, true);
+
+#define NUM_BUTTONS 2
+OneButton buttons[NUM_BUTTONS] = {button1, button2};
+int buttonsIndexes[NUM_BUTTONS] = {0, 1};
+
+void onClick(void *p);
 
 void setup() {
 
-  pinMode(LED, OUTPUT);
-  pinMode(Button1, INPUT);
+  while (!Serial){}
+  
+  Serial.begin(9600);
+
+  for (int i = 0; i < NUM_BUTTONS; i++)
+  {
+    buttons[i].attachClick(onClick, &buttonsIndexes[i]);
+  }
+  
   
 }
 
-void loop() {
-  int ButtonState = digitalRead(Button1);
+void onClick(void *p){
+  int index = *((int *)p);
+  Serial.println("Button ");
+  Serial.println(index + 1);
+  Serial.println(" Pressed");
+}
 
-  if(ButtonState == HIGH)
+void loop() {
+
+  // Serial.println("Hello World");
+  for (int i = 0; i < NUM_BUTTONS; i++)
   {
-    digitalWrite(LED,HIGH);
+    buttons[i].tick();
   }
-  else
-  {
-    digitalWrite(LED, LOW);
-  }
+  delay(10);
 
 }
