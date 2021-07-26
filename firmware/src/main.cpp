8322b48b5d698a12e964d9ccae5a57aae4f49276
Raul Aquino <aquinoraul207@gmail.com>
2021-07-26 12:22:07 -0300

    Buttons via bluetooth worked and functional connections

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
 #include <OneButton.h>
+#include <BleKeyboard.h>
 
-OneButton button1(17, true);
-OneButton button2(23, true);
+BleKeyboard bleKeyboard("Streamdeck", "Raul Aquino", 100);
+
+OneButton button1(23, true);
+OneButton button2(17, true);
 
 #define NUM_BUTTONS 2
 OneButton buttons[NUM_BUTTONS] = {button1, button2};
@@ -21,19 +24,40 @@ void setup() {
     buttons[i].attachClick(onClick, &buttonsIndexes[i]);
   }
   
-  
+  bleKeyboard.begin();
+
 }
 
 void onClick(void *p){
   int index = *((int *)p);
-  Serial.println("Button ");
-  Serial.println(index + 1);
-  Serial.println(" Pressed");
+  Serial.println("Button " + String(index + 1) + " Pressed");
+
+  if(bleKeyboard.isConnected()) {
+    // Serial.println("Sending 'Hello world'...");
+    // bleKeyboard.print("Hello world");
+    switch(index) {
+      case 0:
+        // bleKeyboard.press(KEY_LEFT_ALT);
+        // bleKeyboard.press(KEY_UP_ARROW);
+        // Serial.println("Recording started");
+        bleKeyboard.press(KEY_MEDIA_PREVIOUS_TRACK);
+        Serial.println("Previous track");
+        break;
+      case 1:
+        // bleKeyboard.press(KEY_LEFT_ALT);
+        // bleKeyboard.press(KEY_DOWN_ARROW);
+        // Serial.println("Recording finished");
+        bleKeyboard.press(KEY_MEDIA_NEXT_TRACK);
+        Serial.println("Next track");
+        break;
+      default:
+        break;
+    }
+  }
 }
 
 void loop() {
 
-  // Serial.println("Hello World");
   for (int i = 0; i < NUM_BUTTONS; i++)
   {
     buttons[i].tick();
